07a26b511fed67c35d3e7c02e9109dc648721419
Ji TANG <ji.tang@polytechnique.edu>
2019-11-25 17:21:21 +0100

    add video generator

--- tools.py
+++ tools.py
@@ -2,6 +2,44 @@ import numpy as np
 import random
 import cv2
 import matplotlib.pyplot as plt
+import os
+import glob
+
+def generate_video(decoder, l, outputname):
+    if outputname is None:
+        outputname = 'transition{:3f}-{:3f}'.format(np.min(l), np.max(l))
+    try:
+        os.mkdir('imgs')
+    except:
+        pass
+    pictures = decoder.predict(l)
+    i = 0
+    for p in pictures:
+        plt.ioff()
+        plt.figure(figsize=(28,28), dpi=28)
+        plt.imshow(p.reshape(28, 28))
+        plt.gray()
+        plt.axis('off')
+        plt.savefig('imgs/{:05d}.jpg'.format(i))
+        plt.close()
+        i += 1
+        
+    img_array = []
+    for filename in glob.glob('imgs/*.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+ 
+    try:
+        os.mkdir('videos')
+    except:
+        pass
+    out = cv2.VideoWriter('videos/' + outputname + '.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 30, size)
+
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
 
 def gaussien_noise(images, f=0.1):
     '''
